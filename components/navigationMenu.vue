18b38a3da405b2dfa9ec69ace1e95df83cc68a5f
takex5g <takex5g@gmail.com>
2022-02-05 01:11:23 +0900

    ♻️ ナビゲーションメニューをリファクタリング

--- components/navigationMenu.vue
+++ components/navigationMenu.vue
@@ -1,102 +1,55 @@
 <template>
   <div class="content">
     <br />
-    <table border="0" width="142">
+    <table border="0">
       <tbody>
-        <tr>
-          <td width="15"></td>
-          <td width="10"></td>
-          <td width="148"></td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"><font color="#FFCCCC">●</font></td>
-          <td width="148">
-            <p><a href="top.htm" target="right">トップ</a></p>
+        <tr v-for="sbj in subjects" :key="sbj.name">
+          <td class="pl-3"><span :style="`color:${sbj.color}`">●</span></td>
+          <td>
+            <p class="ma-0 py-3">
+              <nuxt-link :to="sbj.href">{{ sbj.name }}</nuxt-link>
+            </p>
           </td>
         </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"></td>
-          <td width="148"></td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"><font color="#00FF00">●</font></td>
-          <td width="148">
-            <p><a href="movie/eiga.htm" target="right">映画出演</a></p>
-          </td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"></td>
-          <td width="148"></td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"><font color="#33FFFF">●</font></td>
-          <td width="148">
-            <p><a href="tv/tv.htm" target="right">ドラマ出演</a></p>
-          </td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"></td>
-          <td width="148"></td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"><font color="#0099FF">●</font></td>
-          <td width="148">
-            <p><a href="stage/butai.htm" target="right">舞台出演</a></p>
-          </td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"></td>
-          <td width="148"></td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"><font color="#0000FF">●</font></td>
-          <td width="148">
-            <p><a href="photo/photo.htm" target="right">写真集</a></p>
-          </td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"></td>
-          <td width="148"></td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"><font color="#333399">●</font></td>
-          <td width="148">
-            <p><a href="essay/essay.htm" target="right">出版本</a></p>
-          </td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"></td>
-          <td width="148"></td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"><font color="#CC00CC">●</font></td>
-          <td width="148">
-            <p><a href="kanri/kanri.htm" target="right">管理者</a></p>
-          </td>
-        </tr>
-        <tr>
-          <td width="15"></td>
-          <td width="10"></td>
-          <td width="148"></td>
-        </tr>
+        <tr></tr>
       </tbody>
     </table>
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+export type DataType = {
+  subjects: { name: string; color: string; href: string }[]
+}
+export default Vue.extend({
+  components: {},
+  data(): DataType {
+    return {
+      subjects: [
+        { name: 'トップ', color: '#FFCCCC', href: '/' },
+        { name: 'MCCについて', color: '#00FF00', href: '/about' },
+        { name: '活動内容', color: '#33FFFF', href: '/works' },
+        { name: '活動報告', color: '#0099FF', href: '/report' },
+        { name: '新入部員募集', color: ' #0000FF', href: '/recruit' },
+        { name: 'お役立ちリンク', color: ' #333399', href: '/links' },
+        // #CC00CC
+      ],
+    }
+  },
+})
+
+/*
+export default Vue.extend({
+  components: {},
+  layout: 'coefontsLayout',
+  middleware: 'authenticated',
+  data(): DataType {
+    return {}
+  },
+})
+*/
+</script>
 <style lang="scss" scoped>
 .content {
   background-color: #f0f0ff;
