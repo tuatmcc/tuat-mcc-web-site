300293a683b56330fcb81041c9fd4f2d11de6e6d
takex5g <takex5g@gmail.com>
2022-02-06 18:04:52 +0900

    ✨ リンク集

--- pages/links.vue
+++ pages/links.vue
@@ -1,12 +1,37 @@
 <template>
-  <div></div>
+  <div>
+    <table>
+      <tbody>
+        <tr v-for="link in links" :key="link.title">
+          <td class="pl-3"><span>●</span></td>
+          <td>
+            <p class="ma-0 py-3">
+              <a :href="link.to" target="_brank">{{ link.title }}</a>
+            </p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
-<script>
+<script lang="ts">
 import Vue from 'vue'
-// import News from '~/components/top/news.vue'
-
+export type DataType = {
+  subjects: { name: string; href: string }[]
+}
 export default Vue.extend({
   components: {},
+  data(): DataType {
+    return {
+      links: [
+        { title: '東京農工大学', to: 'https://www.tuat.ac.jp/' },
+        {
+          title: '阿部寛のホームページ',
+          to: 'http://abehiroshi.la.coocan.jp/',
+        },
+      ],
+    }
+  },
 })
 </script>
 
