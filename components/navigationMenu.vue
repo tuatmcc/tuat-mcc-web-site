22a72f8681a0be8ba07baf41d941e9d6ef3d059b
takex5g <takex5g@gmail.com>
2022-02-06 17:49:33 +0900

    ♻️ 変数名変更

--- components/navigationMenu.vue
+++ components/navigationMenu.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="content">
-    <br />
     <table border="0">
       <tbody>
-        <tr v-for="sbj in subjects" :key="sbj.name">
+        <tr v-for="sbj in subjects" :key="sbj.title">
           <td class="pl-3"><span :style="`color:${sbj.color}`">●</span></td>
           <td>
             <p class="ma-0 py-3">
-              <nuxt-link :to="sbj.href">{{ sbj.name }}</nuxt-link>
+              <nuxt-link :to="sbj.to">{{ sbj.title }}</nuxt-link>
             </p>
           </td>
         </tr>
-        <tr></tr>
       </tbody>
     </table>
   </div>
@@ -20,35 +18,24 @@
 <script lang="ts">
 import Vue from 'vue'
 export type DataType = {
-  subjects: { name: string; color: string; href: string }[]
+  subjects: { title: string; color: string; to: string }[]
 }
 export default Vue.extend({
   components: {},
   data(): DataType {
     return {
       subjects: [
-        { name: 'トップ', color: '#FFCCCC', href: '/' },
-        { name: 'MCCについて', color: '#00FF00', href: '/about' },
-        { name: '活動内容', color: '#33FFFF', href: '/works' },
-        { name: '活動報告', color: '#0099FF', href: '/news' },
-        { name: '新入部員募集', color: ' #0000FF', href: '/recruit' },
-        { name: 'お役立ちリンク', color: ' #333399', href: '/links' },
+        { title: 'トップ', color: '#FFCCCC', to: '/' },
+        { title: 'MCCについて', color: '#00FF00', to: '/about' },
+        { title: '活動内容', color: '#33FFFF', to: '/works' },
+        { title: '活動報告', color: '#0099FF', to: '/news' },
+        { title: '新入部員募集', color: ' #0000FF', to: '/recruit' },
+        { title: 'お役立ちリンク', color: ' #333399', to: '/links' },
         // #CC00CC
       ],
     }
   },
 })
-
-/*
-export default Vue.extend({
-  components: {},
-  layout: 'coefontsLayout',
-  middleware: 'authenticated',
-  data(): DataType {
-    return {}
-  },
-})
-*/
 </script>
 <style lang="scss" scoped>
 .content {
